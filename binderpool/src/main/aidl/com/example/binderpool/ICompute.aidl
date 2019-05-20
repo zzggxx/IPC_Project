152e4468fa12e82c2a3a739988c4c86d26f6cdbc
zhougaoxiong.newbeiyang <zhougaociongcall@163.com>
2019-05-20 18:08:03 +0800

    Binder连接池

---
+++ binderpool/src/main/aidl/com/example/binderpool/ICompute.aidl
@@ -0,0 +1,8 @@
+// ICompute.aidl
+package com.example.binderpool;
+
+// Declare any non-default types here with import statements
+
+interface ICompute {
+    int add(int a, int b);
+}
---
+++ binderpool/src/main/java/com/example/binderpool/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.binderpool;
+
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "BinderPoolActivity";
+
+    private ISecurityCenter mSecurityCenter;
+    private ICompute mCompute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                doWork();
+            }
+        }).start();
+
+    }
+
+    private void doWork() {
+        BinderPool binderPool = BinderPool.getInsance(MainActivity.this);
+        IBinder securityBinder = binderPool
+                .queryBinder(BinderPool.BINDER_SECURITY_CENTER);
+        ;
+        mSecurityCenter = (ISecurityCenter) SecurityCenterImpl
+                .asInterface(securityBinder);
+        Log.d(TAG, "visit ISecurityCenter");
+        String msg = "helloworld-安卓";
+        System.out.println("content:" + msg);
+        try {
+            String password = mSecurityCenter.encrypt(msg);
+            System.out.println("encrypt:" + password);
+            System.out.println("decrypt:" + mSecurityCenter.decrypt(password));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "visit ICompute");
+        IBinder computeBinder = binderPool
+                .queryBinder(BinderPool.BINDER_COMPUTE);
+        ;
+        mCompute = ComputeImpl.asInterface(computeBinder);
+        try {
+            System.out.println("3+5=" + mCompute.add(3, 5));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
